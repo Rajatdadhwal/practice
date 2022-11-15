80d4597c345d15c8b2ae60327a0fc583b430e1b1
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-11-16 01:46:54 +0530

    practice

---
+++ variable.py
@@ -0,0 +1,26 @@
+#variable
+
+x = 4578
+y = "this is the string"
+z = 10.2
+
+print(x)
+print(y)
+print(y)
+
+#convertion
+
+first = 10.5
+print(int(first))
+second = 45278
+print(float(second))
+
+#concatination
+
+concatination = "Rajat Dadhwal"
+print( "Hello " + concatination)
+
+sentence = "Apple"
+print(sentence   + " a  day " + " keeps the " + " doctor away")
+
+
