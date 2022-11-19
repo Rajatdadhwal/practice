5ed01108253bbe7f210aa7976fbfacdcd6008118
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-11-19 18:53:56 +0530

    adding

---
+++ questions.py
@@ -0,0 +1,16 @@
+#take input and find number of character 
+
+character = input("Enter the character ")
+
+print(len(character))
+
+
+
+
+#take input str and print each word of character 
+
+str = input("enter the input ")
+print(str[0])
+print(str[0:3])
+print(str[:])
+print(str[-1::-1])
